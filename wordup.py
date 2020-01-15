01a1aa36f79e4476dc30a4cbca877cdd10ee0b5d
vesche <vesche@protonmail.com>
2020-01-15 17:15:12 -0600

    fix weigh of another letter

--- wordup.py
+++ wordup.py
@@ -11,7 +11,7 @@ url = 'https://wordfinder.yourdictionary.com/unscramble/{letters}'
 letter_values = {
     'a': 1, 'b': 4, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 3, 'h':  3, 'i': 1,
     'j': 8, 'k': 5, 'l': 2, 'm': 4, 'n': 2, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
-    's': 1, 't': 1, 'u': 2, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+    's': 1, 't': 1, 'u': 2, 'v': 6, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
 
 
